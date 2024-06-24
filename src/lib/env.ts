5db4778984939f7c1d4887adfdb014b416843336
Matheus Bidarra <mathmxb@gmail.com>
2024-06-24 00:39:39 -0300

    starting-project
    initial instances of my ecommerce

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -1,11 +1,16 @@
-import zod from "zod";
+import { z } from "zod";
+import dotenv from "dotenv";
 
-const envSchema = zod.object({
-  DATABASE_URL: zod.string().nonempty(),
-  GOOGLE_CLIENT_ID: zod.string().nonempty(),
-  GOOGLE_CLIENT_SECRET: zod.string().nonempty(),
-  NEXTAUTH_URL: zod.string().nonempty(),
-  NEXTAUTH_SECRET: zod.string().nonempty(),
+dotenv.config();
+
+console.log(process.env);
+
+const envSchema = z.object({
+  DATABASE_URL: z.string().nonempty(),
+  GOOGLE_CLIENT_ID: z.string().nonempty(),
+  GOOGLE_CLIENT_SECRET: z.string().nonempty(),
+  NEXTAUTH_URL: z.string().nonempty(),
+  NEXTAUTH_SECRET: z.string().nonempty(),
 });
 
 export const env = envSchema.parse(process.env);
